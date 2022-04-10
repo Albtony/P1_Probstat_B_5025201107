0c084e19e3f6b1449bcd653bde6662a8e12f0d03
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-04-10 15:31:50 +0700

    finish soal 1

---
+++ soal1/soal1.r
@@ -0,0 +1,30 @@
+pacman::p_load(ggplot2)
+
+# soal 1
+# point a
+x = 3
+p = 0.20
+res = dgeom(x = x, prob = p)
+cat("P(X=3) = ", res)
+
+# point b
+n = 10000
+mean(rgeom(n = n, prob = p) == 3)
+
+# point c
+cat("kesimpulan: probabilitas ditemukannya 3 orang pertama tidak menghadiri acara vaksinasi 
+    mendekati rerata ditemukannya 3 orang pertama tidak menghadiri acara vaksinasi dengan 
+    variabel acak")
+
+# point d
+hist = hist(rgeom(n = n, prob = p), main = "Histogram Distribusi Geometrik")
+png(filename = "hist1.png")
+
+
+# point e
+mean = 1/p
+cat("mean = ", mean)
+
+# point b
+variance = (1-p)/p^2
+cat("variance = ", mean)
