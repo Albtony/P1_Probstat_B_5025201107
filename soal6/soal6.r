618c31ab04a784a93a19cd130904b22f38f9dcd4
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-04-10 20:36:37 +0700

    refactor: improve code readability

--- soal6/soal6.r
+++ soal6/soal6.r
@@ -9,18 +9,21 @@ dist <- rnorm(n, mean = mean, sd = sd)
 x1 <- floor(mean(dist)) - 1
 x2 <- floor(mean(dist)) + 1
 
+z <- (dist - mean(dist))/sd
 z1 <- (x1 - mean)/sd 
 z2 <- (x2 - mean)/sd
 
-z12 = z2 - z1
+z12 = pnorm(z2, mean, sd) - pnorm(z1, mean, sd)
 cat("z score = ", z12)
 
-plot(dist, type="l")
+plot(z, type="l")
 
 # point b
-hist(dist, main = "5025201107_NuzulAbatony_A_DNhistogram")
+hist(dist, breaks = 50, main = "5025201107_NuzulAbatony_A_DNhistogram")
 
 
 # point c
+mean = mean
 variance = sd * sd
+cat("mean = ", mean)
 cat("variance = ", variance)
